d7aefc56499a1f8b67430df1bc7176d16127e7f7
Uttam <uttamrohit4545@gmail.com>
2025-11-16 03:12:25 +0600

    feat : non primitive with tuple

---
+++ src/NonPrimitive.ts
@@ -0,0 +1,10 @@
+const name :string[] = ["uttam","ali","nuru"]
+name.push("")
+
+
+//tuple      //var e redeclare kora jay, const and let e jayna,, const e reassign kora jay na, ar var hocche function scope
+var StudentDetails: [string,string,number] = ["uttam","sharma",10]
+
+
+var StudentDetails1: (string | number)[] = ["uttam","sharma",95]
+
