0918049a9f00dba9bc73f7c76b47efc6defe6140
Uttam <uttamrohit4545@gmail.com>
2025-11-16 11:44:28 +0600

    feat : nonprimitive type function

--- src/NonPrimitive.ts
+++ src/NonPrimitive.ts
@@ -10,7 +10,7 @@ var StudentDetails1: (string | number)[] = ["uttam","sharma",95]
 
 
 //object , literal type , access modifier
-const person : {
+const person1 : {
     // Organization: string;
     // Organization: "Programming Hero"  //value => type = Literal type(so not notun name assignable)
     readonly Organization:string     //access Modifier
@@ -24,9 +24,42 @@ const person : {
     lastName : "Sharma",
     isMarried : true
 }
-person.Organization = "Programming Hero Lite"
+person1.Organization = "Programming Hero Lite"
 console.log(person)
 
+
+
+
+//function
+
+//normal function
+function add(num1 :number,num2:number):number {
+    return num1 + num2
+}
+add(2,4)
+
+
+//arrow function
+const addArrow = (num1:number,num2:number) : number => num1 + num2
+addArrow(2,8)
+
+// method function
+const person = {
+    name : "Uttam Sharma",
+    balance : 0,
+    addBalance(value : number){
+        const newBalance = value + this.balance
+        return newBalance
+    }
+}
+
+person.addBalance(10)
+
+
+//callback function
+const arr : number[] = [1,2,3,4,5]
+const arrSquare = arr.map((ele : number) => ele * ele)
+console.log(arrSquare)
 
 
 
