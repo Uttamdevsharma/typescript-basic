b41194626cd7e017d6eef38954a8f7d916ab6159
Uttam <uttamrohit4545@gmail.com>
2025-11-16 11:12:59 +0600

    feat : object ,literal and access modifier

--- me.js
+++ me.js
@@ -1,3 +1,15 @@
-let name = "uttam"
-name = 123
-console.log(name)
+let person = {
+    name : "Uttam Sharma",
+    addr : {
+        city : 'dhaka',
+        zip : 1800
+    },
+    interest : ['js','typescript','php'],
+    printInterest(){
+        this.interest.forEach((el) => {
+            console.log(this.name + 'Love' + el)
+        })
+    }
+}
+
+person.printInterest()
--- src/NonPrimitive.ts
+++ src/NonPrimitive.ts
@@ -1,10 +1,35 @@
+//Array
 const name :string[] = ["uttam","ali","nuru"]
 name.push("")
-
 
 //tuple      //var e redeclare kora jay, const and let e jayna,, const e reassign kora jay na, ar var hocche function scope
 var StudentDetails: [string,string,number] = ["uttam","sharma",10]
 
-
 var StudentDetails1: (string | number)[] = ["uttam","sharma",95]
+
+
+
+//object , literal type , access modifier
+const person : {
+    // Organization: string;
+    // Organization: "Programming Hero"  //value => type = Literal type(so not notun name assignable)
+    readonly Organization:string     //access Modifier
+    firstName : string;
+    middleName ?: string;
+    lastName: string,
+    isMarried:boolean
+} = {
+    Organization: "Programming Hero",
+    firstName : "Uttam ",
+    lastName : "Sharma",
+    isMarried : true
+}
+person.Organization = "Programming Hero Lite"
+console.log(person)
+
+
+
+
+
+
 
