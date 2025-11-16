befc979597132a2afb31c8a64ceabb6fa3bce546
Uttam <uttamrohit4545@gmail.com>
2025-11-16 23:29:00 +0600

    feat : generic in typescript

---
+++ src/Generic.ts
@@ -0,0 +1,35 @@
+
+//for primitive 
+type GenericArray<value> = Array<value>
+
+const person :GenericArray<string> = ["Uttam","Nuru","Alif"]
+
+const mark :GenericArray<number> = [10,20,30,40]
+
+const isValid: GenericArray<boolean> = [true,false,true,false]
+
+
+
+
+//for tuple
+type Mark<X,Y> = [X,Y]
+const mark : Mark<number,number> = [60,60]
+
+
+
+//for Array of object
+
+const person : GenericArray<{name :string,age:number}> =[
+    {
+        name : "Uttam Sharma",
+        age : 25
+        
+    },
+    {
+        name : "Alif HOssain",
+        age : 26
+    }
+
+
+]
+
